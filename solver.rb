76bc11c9c48d29a06445185bb20215eabb141004
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-09-01 15:53:45 +0300

    Add Fizzbuzz code

--- solver.rb
+++ solver.rb
@@ -15,4 +15,13 @@ class Solver
   def reverse_word(word)
     word.reverse
   end
+
+  def fizzbuzz(number)
+    return 'Is an integer' unless number.is_a? Integer
+    return 'FizzBuzz' if (number % 5).zero? and (number % 3).zero?
+    return 'Fizz' if (number % 3).zero?
+    return 'Buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
